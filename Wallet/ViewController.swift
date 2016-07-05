e6757175c45092c90d5fb75e83f85e2de705f7eb
blennoit <alessandro.distefano1@live.it>
2016-07-05 15:52:06 +0200

    first alert

--- Wallet/ViewController.swift
+++ Wallet/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Create navigation bar item for add new object to the tableview
-         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(ViewController.addNewItem))
+         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(ViewController.launchAlert))
         // Set the title of the view
         title = "I tuoi conti"
         // Populate listaContoCorrente
@@ -49,8 +49,66 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //MARKS: Fuctions
     
-    func addNewItem(){
-        //TO-DO Deve creare l alert per inserire nuovi conti.
+    func launchAlert(){
+        // Example of using the view to add two text fields to the alert
+        // Create the subview
+        let appearance = SCLAlertView.SCLAppearance(
+            kTitleFont: UIFont(name: "HelveticaNeue", size: 20)!,
+            kTextFont: UIFont(name: "HelveticaNeue", size: 14)!,
+            kButtonFont: UIFont(name: "HelveticaNeue-Bold", size: 14)!,
+            showCloseButton: false
+        )
+        
+        // Initialize SCLAlertView using custom Appearance
+        let alert = SCLAlertView(appearance: appearance)
+        
+        // Creat the subview
+        let subview = UIView(frame: CGRectMake(0,0,216,110))
+        let x = (subview.frame.width - 180) / 2
+
+        // Add textfield 1
+        let textfield1 = UITextField(frame: CGRectMake(x, 10,180,25))
+        textfield1.layer.borderColor = UIColor.purpleColor().CGColor
+        textfield1.layer.borderWidth = 1.5
+        textfield1.layer.cornerRadius = 5
+        textfield1.placeholder = "Nome"
+        textfield1.textAlignment = NSTextAlignment.Center
+        subview.addSubview(textfield1)
+        
+        // Add textfield 2
+        let textfield2 = UITextField(frame: CGRectMake(x,textfield1.frame.maxY + 10,180,25))
+        textfield2.secureTextEntry = true
+        textfield2.layer.borderColor = UIColor.purpleColor().CGColor
+        textfield2.layer.borderWidth = 1.5
+        textfield2.layer.cornerRadius = 5
+        textfield1.layer.borderColor = UIColor.purpleColor().CGColor
+        textfield2.placeholder = "Importo"
+        textfield2.textAlignment = NSTextAlignment.Center
+        subview.addSubview(textfield2)
+        
+        // Add textfield 3
+        let textfield3 = UITextField(frame: CGRectMake(x,textfield2.frame.maxY + 10,180,25))
+        textfield3.secureTextEntry = true
+        textfield3.layer.borderColor = UIColor.purpleColor().CGColor
+        textfield3.layer.borderWidth = 1.5
+        textfield3.layer.cornerRadius = 5
+        textfield3.layer.borderColor = UIColor.purpleColor().CGColor
+        textfield3.placeholder = "Tipo conto"
+        textfield3.textAlignment = NSTextAlignment.Center
+        subview.addSubview(textfield3)
+        
+        // Add the subview to the alert's UI property
+        alert.customSubview = subview
+        
+        alert.addButton("Annulla"){
+            print("annulla")
+
+        }
+        alert.addButton("Crea") {
+            print("creato")
+        }
+        
+        alert.showEdit("Conto", subTitle: "Aggiungi un conto!")
     }
     
     func getContoCorrenteFromCoreData() {
